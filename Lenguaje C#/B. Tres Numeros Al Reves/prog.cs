9c2ca47c1b7a475f53e639267e98ce4e64678581
gerardobeltran <contacto@gerardobeltran.mx>
2023-09-26 00:09:12 -0700

    --

---
+++ Lenguaje C#/B. Tres Numeros Al Reves/prog.cs
@@ -0,0 +1,14 @@
+using System;
+class Principal {
+  static void Main() {
+      string num1, num2, num3;
+        // Entrada
+        num1 = Console.ReadLine();
+        num2 = Console.ReadLine();
+        num3 = Console.ReadLine();
+        // Salida
+        Console.WriteLine(num3);
+        Console.WriteLine(num2);
+        Console.WriteLine(num1);
+  }
+}
