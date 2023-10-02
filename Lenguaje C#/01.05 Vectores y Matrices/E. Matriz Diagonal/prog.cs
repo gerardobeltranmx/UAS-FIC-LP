e859e06047963dfd54cfe882cdf3bb3b44eff3fc
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-10-02 10:42:21 -0700

    --

---
+++ Lenguaje C#/01.05 Vectores y Matrices/E. Matriz Diagonal/prog.cs
@@ -0,0 +1,49 @@
+
+using System;
+class Principal {
+    static void Main() {
+   
+      int N;
+      int [,] mat;
+      bool enc=true; 
+      mat = new int [N,N];
+      string linea;
+      string [] datos;
+      // se establece tamaño a la matriz
+      N = int.Parse(Console.ReadLine());
+      // entrada de datos
+      for (int i=0; i<N; i++){
+          linea = Console.ReadLine();
+          datos = linea.Split(" ");
+        for (int j=0; j<N; j++){
+          mat[i,j]=int.Parse(datos[j]);
+        }
+      }
+
+      int valor = mat[0,0];
+
+      for (int i=0; i<N && enc==true; i++){
+          if (mat[i,i]!=valor){
+             enc = false;
+          }
+      }
+      if (enc==true)
+          Console.WriteLine("SI");
+      else
+          Console.WriteLine("NO");    
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+  }
+}
